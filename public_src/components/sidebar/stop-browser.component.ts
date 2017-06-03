58d94d330c2be370ee6d1e35c38fe04450d45c05
David Kocich <dkocich@gmail.com>
2017-06-03 11:34:44 +0200

    wip PA

--- public_src/components/sidebar/stop-browser.component.ts
+++ public_src/components/sidebar/stop-browser.component.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, Input, ElementRef} from "@angular/core";
 import {StorageService} from "../../services/storage.service";
+import {MapService} from "../../services/map.service";
+import {RouteBrowserComponent} from "./route-browser.component";
 
 @Component({
     selector: "stop-browser",
@@ -12,6 +14,27 @@ import {StorageService} from "../../services/storage.service";
 })
 export class StopBrowserComponent {
     private listOfStops: any = this.storageService.listOfStops;
+    el: ElementRef;
 
-    constructor (private storageService: StorageService) {}
+    constructor (el: ElementRef,
+                 private storageService: StorageService,
+                 private mapService: MapService) {
+        this.el = el;
+    }
+
+    // this._elementRef.nativeElement.querySelector("");
+
+    // @Input("routeTableBody") private _routeTableBody: RouteBrowserComponent;
+
+    public exploreRoutesViaStop($event, rel) {
+        // let parentDiv = elementRef.nativeElement.querySelector("routeTableBody");
+
+        const hostElem = this.el.nativeElement;
+        console.dir(hostElem.children, hostElem.parentNode);
+        // console.log($event);
+        // console.log(rel);
+        // if (this.mapService.showRoute(rel)) this.mapService.drawTooltipFromTo(rel);
+
+    }
+
 }
